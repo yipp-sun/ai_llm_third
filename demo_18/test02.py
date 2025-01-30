ef08e6de18de47431cbf9591b3438c30c3c455d3
yipp <song_ontatu@163.com>
2025-01-30 20:12:39 +0800

    upd

--- demo_18/test02.py
+++ demo_18/test02.py
@@ -1,7 +1,7 @@
 from openai import OpenAI
 client = OpenAI(
     api_key='YOUR_API_KEY',
-    base_url="http://0.0.0.0:23333/v1"
+    base_url="http://127.0.0.1:23333/v1"
 )
 model_name = client.models.list().data[0].id
 response = client.chat.completions.create(
--- demo_18/test03.py
+++ demo_18/test03.py
@@ -1,5 +1,5 @@
 #模型下载
 from modelscope import snapshot_download
-model_dir = snapshot_download('Qwen/Qwen1.5-1.8B-Chat',cache_dir="/root/autodl-tmp/1lm/")
+model_dir = snapshot_download('Qwen/Qwen1.5-1.8B-Chat',cache_dir="/root/autodl-tmp/llm/")
 
 
