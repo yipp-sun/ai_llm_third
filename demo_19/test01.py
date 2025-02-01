9cf1e58c9744dffab02d41bf02ceaf43cde95e62
yipp <song_ontatu@163.com>
2025-02-01 17:09:16 +0800

    upd

---
+++ demo_19/test01.py
@@ -0,0 +1,7 @@
+from datasets import load_dataset,load_from_disk
+
+traindata = load_dataset('ptb_text_only', 'penn_treebank', split='train', trust_remote_code=True)
+valdata = load_dataset('ptb_text_only', 'penn_treebank', split='validation', trust_remote_code=True)
+
+traindata.save_to_disk('ptb_train')
+valdata.save_to_disk('ptb_val')
